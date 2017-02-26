1db7362210bcec69ed1249dff802b6625aeb80d4
vincent-van-hoef <vvanhoef@hotmil.com>
2017-02-26 20:54:11 +0100

    Clean up third import function

--- Src/collectData.R
+++ Src/collectData.R
@@ -74,18 +74,9 @@ columnCheck(listOfFiles)
 rowCheck(listOfFiles)
 nameCheck(listOfFiles)
 
-# Clean up names and collecct to assign to the files in long format
-namesOfFiles <- list.files(path=path, pattern = "*.csv")
-namesOfFiles <- spaceToUnderscore(namesOfFiles)
-namesOfFiles <- removePattern(namesOfFiles, pattern="_PB_pre_post.csv")
-namesOfFiles <- removePattern(namesOfFiles, pattern="_pre_post.csv")
-
-# define function to convert from wide to long format
-listOfFilesLong <- lapply(listOfFiles, tidyData)
-
 # assign the correct measurement measurement
-for(i in 1:length(listOfFilesLong)){
-  names(listOfFilesLong[[i]]) <- c("Patient", "Sample", namesOfFiles[i])
+for(i in 1:length(listOfFiles)){
+  names(listOfFiles[[i]])[c(1,2)] <- c("Patient", "Sample")
 }
 
-df_pre_post <- cbind(listOfFilesLong[[1]][c(1,2)], do.call(cbind.data.frame, lapply(listOfFilesLong, function(x) x[3])))
+df_tum <- cbind(listOfFiles[[1]][c(1,2)], do.call(cbind.data.frame, lapply(listOfFiles, function(x) x[c(3,4)])))
