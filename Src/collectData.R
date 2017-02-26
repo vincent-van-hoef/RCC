379ebe684445c83e65c0aa97169a9d1e6ff47a6a
vincent-van-hoef <vvanhoef@hotmil.com>
2017-02-26 19:21:59 +0100

    Added second class of .csv files

--- Src/collectData.R
+++ Src/collectData.R
@@ -2,33 +2,58 @@ library(dplyr)
 library(tidyr)
 library(WriteXLS)
 
+# 1st set of data
+path <- "./Data/NK activation/PB_pre vs Tum/"
 # Import data files and store in a list
-listOfFiles <- lapply(list.files(path="./Data/NK activation/PB_pre vs Tum/", pattern="*.csv", full.names = TRUE), function(x) read.csv2(x, sep=",", dec = "."))
+listOfFiles <- lapply(list.files(path=path, pattern="*.csv", full.names = TRUE), function(x) read.csv2(x, sep=",", dec = "."))
 
 # Validate files
 columnCheck(listOfFiles)
 rowCheck(listOfFiles)
+nameCheck(listOfFiles)
 
-namesOfFiles <- list.files(pattern = "*.csv")
-varNames <- gsub(" ", "_", namesOfFiles)
-varNames <- gsub("_PB_tum.csv", "", varNames)
+# Clean up names and collecct to assign to the files in long format
+namesOfFiles <- list.files(path=path, pattern = "*.csv")
+namesOfFiles <- spaceToUnderscore(namesOfFiles)
+namesOfFiles <- removePattern(namesOfFiles, pattern="_PB_tum.csv")
 
 # define function to convert from wide to long format
-tidyData <- function(x){
-  x %>% gather(Sample, value, -1)
-}
+listOfFilesLong <- lapply(listOfFiles, tidyData)
 
-listOfFilesLong <- lapply(listOfFiles, function(x) tidyData(x))
-listOfFilesLong <- lapply(listOfFilesLong, function(x) {names(x)[1] <- "Patient"; x})
-listOfFilesLong <- lapply(listOfFilesLong, function(x) {x[x[,2]=="PB", 2] <- "PB_pre"; x})
 # assign the correct measurement measurement
 for(i in 1:length(listOfFilesLong)){
-  names(listOfFilesLong[[i]])[3] <- varNames[i]
+  names(listOfFilesLong[[i]]) <- c("Patient", "Sample", namesOfFiles[i])
 }
 
-fullDF <- Reduce(function(...) merge(..., all=TRUE), listOfFilesLong)
-WriteXLS(fullDF, "allData.xls")
+df <- cbind(listOfFilesLong[[1]][c(1,2)], do.call(cbind.data.frame, lapply(listOfFilesLong, function(x) x[3])))
 
+# 2nd set of data
+path <- "./Data/NK activation/PB_pre vs PB_post/"
+# Import data files and store in a list
+listOfFiles <- lapply(list.files(path=path, pattern="*.csv", full.names = TRUE), function(x) read.csv2(x, sep=",", dec = "."))
 
+# Validate files
+columnCheck(listOfFiles)
+rowCheck(listOfFiles)
+nameCheck(listOfFiles)
 
+# Rename
+# assign the correct measurement measurement
+for(i in 1:length(listOfFiles)){
+  names(listOfFiles[[i]]) <- c("X", "PB.pre", "PB.post")
+}
 
+# Clean up names and collecct to assign to the files in long format
+namesOfFiles <- list.files(path=path, pattern = "*.csv")
+namesOfFiles <- spaceToUnderscore(namesOfFiles)
+namesOfFiles <- removePattern(namesOfFiles, pattern="_PB_pre_post.csv")
+
+# define function to convert from wide to long format
+listOfFilesLong <- lapply(listOfFiles, tidyData)
+
+# assign the correct measurement measurement
+for(i in 1:length(listOfFilesLong)){
+  names(listOfFilesLong[[i]]) <- c("Patient", "Sample", namesOfFiles[i])
+}
+
+df <- cbind(listOfFilesLong[[1]][c(1,2)], do.call(cbind.data.frame, lapply(listOfFilesLong, function(x) x[3])))
