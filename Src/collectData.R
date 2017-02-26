d5b79e3e2d6bbd7533815d51bc9815e5c822aa21
vincent-van-hoef <vvanhoef@hotmil.com>
2017-02-26 14:10:31 +0100

    First commit

---
+++ Src/collectData.R
@@ -0,0 +1,32 @@
+library(dplyr)
+library(tidyr)
+library(WriteXLS)
+
+# Import data files and store in a list
+listOfFiles <- lapply(list.files(pattern="*.csv"), function(x) read.csv2(x, sep=",", dec = "."))
+# Check names
+#lapply(listOfFiles, names)
+# Collect names of files and modify into appropriate measurement names
+namesOfFiles <- list.files(pattern = "*.csv")
+varNames <- gsub(" ", "_", namesOfFiles)
+varNames <- gsub("_PB_tum.csv", "", varNames)
+
+# define function to convert from wide to long format
+tidyData <- function(x){
+  x %>% gather(Sample, value, -1)
+}
+
+listOfFilesLong <- lapply(listOfFiles, function(x) tidyData(x))
+listOfFilesLong <- lapply(listOfFilesLong, function(x) {names(x)[1] <- "Patient"; x})
+listOfFilesLong <- lapply(listOfFilesLong, function(x) {x[x[,2]=="PB", 2] <- "PB_pre"; x})
+# assign the correct measurement measurement
+for(i in 1:length(listOfFilesLong)){
+  names(listOfFilesLong[[i]])[3] <- varNames[i]
+}
+
+fullDF <- Reduce(function(...) merge(..., all=TRUE), listOfFilesLong)
+WriteXLS(fullDF, "allData.xls")
+
+
+
+
