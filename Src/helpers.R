12603c914302fcb9ad0ea428e84f659cfd894a6a
vincent-van-hoef <vvanhoef@hotmil.com>
2017-02-26 15:12:18 +0100

    Add row and column check

--- Src/helpers.R
+++ Src/helpers.R
@@ -1,4 +1,21 @@
-# Check whether the files have the number of columns specified in the columns parameter
-columnCheck <- function(x, columns = 3){
-  dim(x)[2]==columns
+# Do all files in a list of files have the same number of columns?
+columnCheck <- function(x){
+  col <- sapply(x, function(y) dim(y)[2])
+  if(all.equal(min(col), max(col))){
+    print("All files have the same number of columns")
+  } else 
+    {
+  print("Not all files have the same number of columns")
+  }
+}
+
+# Do all files in a list of files have the same number of rows?
+rowCheck <- function(x){
+  col <- sapply(x, function(y) dim(y)[1])
+  if(all.equal(min(col), max(col))){
+    print("All files have the same number of rows")
+  } else 
+  {
+    print("Not all files have the same number of rows")
+  }
 }
